e1ea034f0618a06f437947d56bb3308b191c0b02
AnushkaMenon <30318462+AnushkaMenon@users.noreply.github.com>
2020-04-14 01:39:28 +1000

    Added comments

--- take_photo/rubber_ducky_take_photo_windows.ino
+++ take_photo/rubber_ducky_take_photo_windows.ino
@@ -7,23 +7,35 @@ void setup() {
 
 bool finished = false;
 void loop() {
-  // put your main code here, to run repeatedly:
   if (not finished) {
     DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.delay(500);
+    
+    // open the Run window
+    // shortcut: Windows Key + R
+    // allow a delay for the window to open
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
     DigiKeyboard.delay(2500);
+    
+    // open the Camera application
+    // allow a 6 second delay for the application to oepn
     DigiKeyboard.print("microsoft.windows.camera:");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(6000);
+    
+    // take a picture
+    // shorcut: Enter
+    // allow a delay for the picture to load in the library
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(4000);
 
+    // shortcut to open the library to see the picture we just took: press the Tab key 8 times
     for (int i = 0; i < 8; i++) {
       DigiKeyboard.sendKeyStroke(43);
       DigiKeyboard.delay(200);
     }
 
+    // Open the library to view the picture
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
     finished = true;
