b61ed0e65d31fe59043185544b6b43c82b96451f
AnushkaMenon <30318462+AnushkaMenon@users.noreply.github.com>
2020-04-14 01:31:19 +1000

    Updated delay time

--- lock_screen/rubber_ducky_lock_screen_windows.ino
+++ lock_screen/rubber_ducky_lock_screen_windows.ino
@@ -6,11 +6,10 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   DigiKeyboard.sendKeyStroke(0);
 
   // lock screen every two minutes
-  DigiKeyboard.delay(10000);
+  DigiKeyboard.delay(120000);
 
   // keyboard shortcut to lock screen: Windows Key + L
   DigiKeyboard.sendKeyStroke(KEY_L, MOD_GUI_LEFT);
