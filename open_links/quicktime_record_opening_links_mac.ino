801e7238091f21b3aa4eb22a26c4ab6c8341b3eb
AnushkaMenon <30318462+AnushkaMenon@users.noreply.github.com>
2020-04-14 02:15:08 +1000

    Update and rename rubber_ducky_quicktime_record_link_loop.ino to quicktime_record_opening_links_mac.ino

--- open_links/quicktime_record_opening_links_mac.ino
+++ open_links/quicktime_record_opening_links_mac.ino
@@ -17,26 +17,29 @@ void setup() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
+  
   // shortcut to start a new recording is Command + Ctrl + N
   DigiKeyboard.sendKeyStroke(KEY_N, MOD_CONTROL_LEFT | MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
+  
   // Press space to start a new recording
   DigiKeyboard.sendKeyStroke(KEY_SPACE);
+  
   // We have to manually click to actually start the recording, so allow a time delay for this
   DigiKeyboard.delay(3000);
 
   // increase volume
-  /*for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++) {
     DigiKeyboard.sendKeyStroke(128)
-  }*/
+  }
 
-  // open spotlight and type in link
+  // open spotlight and type in the link
   DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
   DigiKeyboard.print("http://youtu.be/dQw4w9WgXcQ");
   DigiKeyboard.delay(500);
   
-  // copy link so that we can quickly use it again later
+  // copy the link so that we can quickly use it again later
   DigiKeyboard.sendKeyStroke(KEY_A, MOD_GUI_LEFT);
   DigiKeyboard.sendKeyStroke(KEY_C, MOD_GUI_LEFT);
 
@@ -48,7 +51,6 @@ int count = 0;
 bool finished = false;
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if (not finished) {
     // open 30 more windows!
     if (count < 5) {
@@ -66,11 +68,12 @@ void loop() {
       
     } else {
       // we've opened up 31 windows playing the same video, now stop the recording
-      // allow a delay so that we can see the result of the script
+      // allow for a delay so that we can see the result of the script
       DigiKeyboard.delay(10000);
       // shortcut to stop the recording is Command + Control + Escape
       //  (usage ID of the Escape key is 41)
       DigiKeyboard.sendKeyStroke(41, MOD_CONTROL_LEFT | MOD_GUI_LEFT);
+      
       finished = true;
     }
     count++;
